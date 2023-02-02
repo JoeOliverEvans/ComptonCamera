880c28268c1c045a9ff24fdaedab52ce15030540
joeol <joeoliverevans@gmail.com>
2023-02-02 19:49:29 +0000

    Made class for detected pairs

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -1,32 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.constants
+import scipy.constants as constants
 
 
-# TODO define class of coincident detections
+electron_mass = (constants.electron_mass * constants.c ** 2) / (constants.electron_volt * 10 ** 3)  # in keV
 
-electron_mass = scipy.constants.electron_mass()
+def CalculateScatterAngle(InitialEnergy, AbsorptionEnergy):
+    """
+    :param InitialEnergy: Photon energy emitted by the source
+    :param AbsorptionEnergy: Photon energy at the final detector
+    :return: Compton Scattering Angle in degrees
+    """
+    return np.arccos(
+        1 - (electron_mass * ((InitialEnergy - AbsorptionEnergy) / (InitialEnergy * AbsorptionEnergy)))) * 180 / np.pi
 
 
 class DetectionPair:
-    def __init__(self, xScatter, yScatter, zScatter, xAbsorption, yAbsorption, zAdsorption, InitialEnergy, AbsorptionEnergy):
+    def __init__(self, xScatter, yScatter, zScatter, xAbsorption, yAbsorption, zAdsorption, InitialEnergy,
+                 AbsorptionEnergy):
+        """
+        :param xScatter: Coordinates of scatter
+        :param yScatter:
+        :param zScatter:
+        :param xAbsorption: Coordinates of absorption
+        :param yAbsorption:
+        :param zAdsorption:
+        :param InitialEnergy:
+        :param AbsorptionEnergy:
+        """
         self.ScatterPosition = [xScatter, yScatter, zScatter]
         self.AbsorptionPosition = [xAbsorption, yAbsorption, zAdsorption]
-        self.LineEquation =
+        self.LineVector = np.array(self.ScatterPosition) - np.array(self.AbsorptionPosition)
         self.AbsorptionEnergy = AbsorptionEnergy
-        self.ScatterAngle = self.CalculateScatterAngle(InitialEnergy, AbsorptionEnergy)
+        self.ScatterAngle = CalculateScatterAngle(InitialEnergy, AbsorptionEnergy)
 
-    def CalculateScatterAngle(self, InitialEnergy, AbsorptionEnergy):
-        """
-        :param InitialEnergy: Photon energy emitted by the source
-        :param AbsorptionEnergy: Photon energy at the final detector
-        :return: Compton Scattering Angle
-        """
-        return np.arccos(1 - electron_mass * ((1/AbsorptionEnergy) - (1/InitialEnergy)))
+
+firstpair = DetectionPair(1, 1, 1, 0, 0, 0, 500, 180)
+print(firstpair.ScatterAngle)
+print(firstpair.LineVector)
 
 
 
-
+'''
 x, y, z = np.indices((8, 8, 8))
 
 # draw cuboids in the top left and bottom right corners, and a link between
@@ -49,3 +64,4 @@ ax = plt.figure().add_subplot(projection='3d')
 ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
 
 plt.show()
+'''
