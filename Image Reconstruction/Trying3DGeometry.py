32acae4f8e59aab0a6e024d6913b126d90664223
Joe Evans <joeoliverevans@gmail.com>
2023-02-05 18:36:51 +0000

    added a second cone and found intersection voxels colored green on the matplotlib, takes a while to run but this is because of the plot not the intersection calculation

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as constants
-import matplotlib; matplotlib.use("TkAgg")
+#import matplotlib; matplotlib.use("TkAgg")
 
 electron_mass = (constants.electron_mass * constants.c ** 2) / (constants.electron_volt * 10 ** 3)  # in keV
 
@@ -33,7 +33,11 @@ class DetectionPair:
 
 pairs = []
 firstpair = DetectionPair([31, 31, 1], [30, 30, 0], 500, 400)
+secondpair = DetectionPair([71, 71, 1], [80, 80, 0], 500, 300)
+#thirdpair = DetectionPair([10, 71, 1], [10, 80, 0], 500, 200)
 pairs.append(firstpair)
+pairs.append(secondpair)
+#pairs.append(thirdpair)
 print(firstpair.scatterAngle)
 print(firstpair.lineVector)
 
@@ -41,8 +45,7 @@ imaging_area = 100  # m
 voxel_length = 1 * 10 ** (0)  # m
 voxels_per_side = int(imaging_area / voxel_length)
 print(voxels_per_side)
-voxel_cube = np.full((voxels_per_side, voxels_per_side, voxels_per_side), False)
-
+voxel_cube = np.zeros((voxels_per_side, voxels_per_side, voxels_per_side), dtype=int)
 
 for pair in pairs:
     for x in np.arange(0, imaging_area, voxel_length):
@@ -52,37 +55,46 @@ for pair in pairs:
                 * (np.tan(pair.scatterAngle)) ** 2)
             z_argument = z // voxel_length
             if z_argument < voxels_per_side:
-                voxel_cube[(x, y, int(z_argument))] = True
-#print(voxel_cube)
-success = False
-print(voxel_cube)
+                voxel_cube[(x, y, int(z_argument))] = voxel_cube[(x, y, int(z_argument))] + 1
+
+
 '''for pair in pairs:
     x = np.arange(0, imaging_area, voxel_length)
     y = np.arange(0, imaging_area, voxel_length)'''
 
-print(np.shape(voxel_cube))
 
 x, y, z = np.indices((8, 8, 8))
 
 # draw cuboids in the top left and bottom right corners, and a link between
-# them
-cube1 = (x < 3) & (y < 3) & (z < 3)
-cube2 = (x >= 5) & (y >= 5) & (z >= 5)
-#link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
-print(cube2)
-# combine the objects into a single boolean array
-voxelarray = voxel_cube
 
-print(voxelarray)
+
+
+# combine the objects into a single boolean array
+
+print(np.max(voxel_cube))
+
 # set the colors of each object
-colors = np.empty(voxelarray.shape, dtype=object)
-#colors[link] = 'red'
-colors[voxelarray] = 'blue'
-#colors[cube2] = 'green'
+colors = np.empty(voxel_cube.shape, dtype=object)
+cones = np.where(voxel_cube < 1, voxel_cube, False)
 
 # and plot everything
-ax = plt.figure().add_subplot(projection='3d')
-ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
+view_only_intersections = False
+if view_only_intersections == True:
+    intersections = voxel_cube > 1
+    # intersections = np.where(voxel_cube > 1, voxel_cube, True)
+    intersections = np.array(intersections, dtype=bool)
+    colors[intersections] = 'green'
 
-plt.show()
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(intersections, facecolors=colors, edgecolor='k')
+
+    plt.show()
+else:
+    colors[voxel_cube == 1] = 'blue'
+    colors[voxel_cube > 1] = 'green'
+
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(voxel_cube, facecolors=colors, edgecolor='k')
+
+    plt.show()
 
