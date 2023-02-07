7be1c00f702f58816c5b5fde31598bf4a14fe41a
joeol <joeoliverevans@gmail.com>
2023-02-07 17:45:46 +0000

    added tilt to cones(i think) may not be correct, needs verifying

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as constants
-#import matplotlib; matplotlib.use("TkAgg")
+import matplotlib; matplotlib.use("TkAgg")
 
 electron_mass = (constants.electron_mass * constants.c ** 2) / (constants.electron_volt * 10 ** 3)  # in keV
 
@@ -32,16 +32,16 @@ class DetectionPair:
 
 
 pairs = []
-firstpair = DetectionPair([31, 31, 1], [30, 30, 0], 500, 400)
-secondpair = DetectionPair([71, 71, 1], [80, 80, 0], 500, 300)
+firstpair = DetectionPair([21, 25, 5], [21, 21, 0], 500, 400)
+#secondpair = DetectionPair([71, 71, 1], [80, 80, 0], 500, 300)
 #thirdpair = DetectionPair([10, 71, 1], [10, 80, 0], 500, 200)
 pairs.append(firstpair)
-pairs.append(secondpair)
+#pairs.append(secondpair)
 #pairs.append(thirdpair)
 print(firstpair.scatterAngle)
 print(firstpair.lineVector)
 
-imaging_area = 100  # m
+imaging_area = 50  # m
 voxel_length = 1 * 10 ** (0)  # m
 voxels_per_side = int(imaging_area / voxel_length)
 print(voxels_per_side)
@@ -50,14 +50,17 @@ voxel_cube = np.zeros((voxels_per_side, voxels_per_side, voxels_per_side), dtype
 for pair in pairs:
     for x in np.arange(0, imaging_area, voxel_length):
         for y in np.arange(0, imaging_area, voxel_length):
-            z = pair.scatterPosition[2] + np.sqrt(
+            beta = np.arctan(pair.lineVector[0]/pair.lineVector[2])
+            gamma = np.arctan(pair.lineVector[1]/pair.lineVector[2])
+            #x_prime = np.cos(beta)*x + np.sin(beta)*np.sin(gamma)*y + np.sin(beta)*np.cos(gamma)*z
+            z = (- (np.cos(beta) * np.sin(gamma) * y) + np.sin(gamma) * x + pair.scatterPosition[2] + np.sqrt(
                 ((x - pair.scatterPosition[0]) ** 2 + (y - pair.scatterPosition[1]) ** 2)
-                * (np.tan(pair.scatterAngle)) ** 2)
+                * (np.tan(pair.scatterAngle)) ** 2))/(np.cos(beta)*np.cos(gamma))
             z_argument = z // voxel_length
-            if z_argument < voxels_per_side:
+            if 0 <= z_argument < voxels_per_side:
                 voxel_cube[(x, y, int(z_argument))] = voxel_cube[(x, y, int(z_argument))] + 1
 
-
+print(voxel_cube[21, 25, 5])
 '''for pair in pairs:
     x = np.arange(0, imaging_area, voxel_length)
     y = np.arange(0, imaging_area, voxel_length)'''
@@ -78,7 +81,7 @@ colors = np.empty(voxel_cube.shape, dtype=object)
 cones = np.where(voxel_cube < 1, voxel_cube, False)
 
 # and plot everything
-view_only_intersections = False
+'''view_only_intersections = False
 if view_only_intersections == True:
     intersections = voxel_cube > 1
     # intersections = np.where(voxel_cube > 1, voxel_cube, True)
@@ -94,7 +97,11 @@ else:
     colors[voxel_cube > 1] = 'green'
 
     ax = plt.figure().add_subplot(projection='3d')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
     ax.voxels(voxel_cube, facecolors=colors, edgecolor='k')
 
     plt.show()
 
+'''
