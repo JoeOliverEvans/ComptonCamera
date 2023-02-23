727ede805ce11c687c5802aec82299a54cb9d45a
amberlaurenj <124282007+amberlaurenj@users.noreply.github.com>
2023-02-23 15:41:35 +0000

    Only positive half

--- Image Reconstruction/WorkingWithPolars.py
+++ Image Reconstruction/WorkingWithPolars.py
@@ -7,27 +7,21 @@ Created on Thu Feb 16 14:43:24 2023
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
+N = 50
 theta_c = 0.65
-# Create the mesh in polar coordinates and compute corresponding Z.
-r = np.linspace(-1, 1, 50)
-theta = np.linspace(0, 2*np.pi, 50)
+
+# Create the mesh in polar coordinates and compute corresponding Z
+r = np.linspace(0, 1, N)
+theta = np.linspace(0, 2*np.pi, N)
 R, Theta = np.meshgrid(r, theta)
 Z = (1/np.tan(theta_c))*R
-# Express the mesh in the cartesian system.
+# Express the mesh in the cartesian system
 X, Y = R*np.cos(Theta), R*np.sin(Theta)
+# Plot the surface
+ax.plot_surface(X, Y, Z, cmap=plt.cm.twilight_shifted)
 
-# Plot the surface.
-ax.plot_surface(X, Y, Z, cmap=plt.cm.YlGnBu_r)
-
-# Tweak the limits and add latex math labels.
-ax.set_zlim(-1, 1)
-ax.set_xlabel(r'$\phi_\mathrm{real}$')
-ax.set_ylabel(r'$\phi_\mathrm{im}$')
-ax.set_zlabel(r'$V(\phi)$')
-
+ax.set_zlim(0, 1)
 plt.show()
-
