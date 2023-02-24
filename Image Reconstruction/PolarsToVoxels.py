c9b07df4a65aa296122a1c82235b9cebbb99c14b
joeol <joeoliverevans@gmail.com>
2023-02-24 12:41:33 +0000

    points are now in 2D arrays with a weighting as the last value of the four vector

--- Image Reconstruction/PolarsToVoxels.py
+++ Image Reconstruction/PolarsToVoxels.py
@@ -10,38 +10,28 @@ import numpy as np
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-N = 50
 theta_c = (np.pi/4)-0.001
-voxel_length = 1
-R_max = 4
-# Create the mesh in polar coordinates and compute corresponding Z
-r = np.linspace(0, 1, N)
+voxel_length = 5
+R_max = 60
 
-theta = np.linspace(0, 2*np.pi, N)
 
-R = np.arange(0, R_max, voxel_length/4)
+R = np.arange(0, R_max, voxel_length/2)
 X = []
 Y = []
 Z = []
+points = np.array([0,0,0,0])
+weight = 1
 for r in R:
-    Theta_size = 2 * np.arctan((voxel_length/4)/(np.sin(theta_c) * r))
-    print(Theta_size)
+    Theta_size = 2 * np.arctan((voxel_length/2)/(np.sin(theta_c) * r))
     Theta = np.linspace(0, 2 * np.pi, int(np.abs(2*np.pi//Theta_size) + 1))
-    for x in r*np.sin(Theta):
-        X.append(x)
-    for y in r*np.cos(Theta):
-        Y.append(y)
-        Z.append(np.cos(theta_c)*r)
-print(X)
-print(np.shape(X))
-print(Y)
-print(np.shape(Y))
-print(Z)
-print(np.shape(Z))
+    for t in Theta:
+        point = np.array([r * np.sin(t), r * np.cos(t), np.cos(theta_c) * r, weight])
+        points = np.row_stack((points, point))
 
+points = np.delete(points, 0, axis=0)
 
 # Plot the surface
-ax.scatter(X, Y, Z, c=Z, cmap=plt.cm.twilight_shifted)
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=points[:, 2], cmap=plt.cm.twilight_shifted)
 
-ax.set_zlim(0, 2)
+ax.set_zlim(0, 40)
 plt.show()
