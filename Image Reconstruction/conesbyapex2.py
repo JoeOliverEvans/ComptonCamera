4dbdb4613f54784b1f1082b0ab8849fd0a812943
CaraClarke2 <124282339+CaraClarke2@users.noreply.github.com>
2023-02-27 13:10:47 +0000

    Add files via upload
    Looks slighty better (like a cone) when the vector is (0, 1, 1)

--- Image Reconstruction/conesbyapex2.py
+++ Image Reconstruction/conesbyapex2.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 27 10:36:59 2023
+Created on Mon Feb 27 12:45:56 2023
 
-@author: JackHocking
+@author: Cara Clarke
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-# def cone():
-#     a = 0.5
-#     b = -1
-#     c = 0.5*(1 - X**2 - Y**2)
-#     Z = (-b + np.sqrt(abs((b ** 2) - (4 * (a) * c))) / np.float(2 * (a)))
-#     return Z                                     
-         
+
 def cone(a1, a2, a3, v1, v2, v3, theta, X, Y):
-    a = (v3**2-(np.cos(theta))**2)
-    b = 2*(v1*v3*a1+v2*v3*a1-v1*v3*X-v2*v3*Y-a3*(np.cos(theta))**2)
-    c = (X**2-2*a1*X+a1**2+Y**2-2*a2*Y+a2**2+a3**2)*(np.cos(theta))**2 - (v1**2)*(X-a1)**2 - (v2**2)*(Y-a2)**2 - (a3**2)*(v3**2) - 2*v1*v2*(X*Y - a2*a1 - X*a2 - Y*a1) + 2*v1*v3*a1*a3 + 2*v1*v3*X*a3 - 2*v2*v3*a2*a3 + 2*v2*v3*Y*a3
-    Z = (-b + np.sqrt(abs((b ** 2) - (4 * (a) * c))) / np.float(2 * (a)))
-    return Z                                                                     
+    a = (v3**2 - (np.cos(theta))**2)
+    b = 2*(v1*v3*X - v1*v3*a1 - (v3**2)*a3 + v2*v3*Y - v2*v3*a2 + a3*(np.cos(theta))**2) #2*(v1*v3*a1 + v2*v3*a2 - v1*v3*X - v2*v3*Y - a3*(np.cos(theta))**2)
+    c = (v3**2)*(a3**2) + 2*v1*v3*a3*(a1-X) + 2*v2*v3*a3*(a2-Y) + (v1**2)*((X-a1)**2) + (v2**2)*((Y-a2)**2) + 2*v1*v2*(X-a1)*(Y-a2) - ((np.cos(theta))**2)*((a3**2) + (X-a1)**2 + (Y-a2)**2) #(X**2 - 2*a1*X + a1**2 + Y**2 - 2*a2*Y + a2**2 + a3**2) * (np.cos(theta))**2 - (v1**2)*(X-a1)**2 - (v2**2)*(Y-a2)**2 - (a3**2)*(v3**2) - 2*v1*v2*(X*Y - a2*a1 - X*a2 - Y*a1) + 2*v1*v3*a1*a3 + 2*v1*v3*X*a3 - 2*v2*v3*a2*a3 + 2*v2*v3*Y*a3
+    Z = (-b + np.sqrt(abs((b ** 2) - (4 * (a) * c)))) / np.float(2 * (a))
+    return Z
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 x = np.arange(-50.0, 50, 1)
 y = np.arange(-50.0, 50, 1)
 X, Y = np.meshgrid(x, y)
-Z = cone(0, 0, 0, 1, 1, 1, np.pi/4, X, Y)
-
+Z = cone(0, 0, 0, 0, 1, 1, np.pi/6, X, Y)
 
 ax.plot_surface(X, Y, Z)
 
