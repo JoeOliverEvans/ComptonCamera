fdbe04303dad11680574ff25c12053bb0db0eb44
joeol <joeoliverevans@gmail.com>
2023-02-28 17:42:08 +0000

    upload for amber

--- Image Reconstruction/PolarsToVoxels.py
+++ Image Reconstruction/PolarsToVoxels.py
@@ -10,7 +10,7 @@ import numpy as np
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-theta_c = (np.pi/4)-0.001
+theta_c = (np.pi/7)-0.001
 voxel_length = 5
 R_max = 60
 
@@ -22,10 +22,10 @@ Z = []
 points = np.array([0,0,0,0])
 weight = 1
 for r in R:
-    Theta_size = 2 * np.arctan((voxel_length/2)/(np.sin(theta_c) * r))
+    Theta_size = 2 * (1/np.tan((voxel_length/2))/(np.sin(theta_c) * r))
     Theta = np.linspace(0, 2 * np.pi, int(np.abs(2*np.pi//Theta_size) + 1))
     for t in Theta:
-        point = np.array([r * np.sin(t), r * np.cos(t), np.cos(theta_c) * r, weight])
+        point = np.array([r * np.sin(t), r * np.cos(t), (1/np.tan(theta_c) * r), weight])
         points = np.row_stack((points, point))
 
 points = np.delete(points, 0, axis=0)
