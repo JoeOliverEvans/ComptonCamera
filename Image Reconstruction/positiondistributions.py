f6a647c31956c50546031e59efd2b5eefdadfe6b
CaraClarke2 <124282339+CaraClarke2@users.noreply.github.com>
2023-03-23 16:40:31 +0000

    Add files via upload

--- Image Reconstruction/positiondistributions.py
+++ Image Reconstruction/positiondistributions.py
@@ -8,11 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as stats
+from tabulate import tabulate
 
 #get data
-df = pd.read_csv('result.csv', sep=';')
-df.to_parquet('result.parquet')
-df2 = pd.read_parquet('result.parquet') 
+df = pd.read_csv('newdata.csv', sep=';')
+df.to_parquet('newdata.parquet')
+df2 = pd.read_parquet('newdata.parquet') 
 det = df2['CHANNEL']
 X = df2['X']
 Y = df2['Y']
@@ -47,6 +48,76 @@ d_5 = df2.query('CHANNEL == 4')
 d_6 = df2.query('CHANNEL == 5')
 d_7 = df2.query('CHANNEL == 6')
 d_8 = df2.query('CHANNEL == 7')
+
+
+
+#detector absorption
+d_1_a = abs_pos.query('CHANNEL == 0')
+x_a_1 = d_1_a['X']
+y_a_1 = d_1_a['Y']
+z_a_1 = d_1_a['Z']
+d_2_a = abs_pos.query('CHANNEL == 1')
+x_a_2 = d_2_a['X']
+y_a_2 = d_2_a['Y']
+z_a_2 = d_2_a['Z']
+d_3_a = abs_pos.query('CHANNEL == 2')
+x_a_3 = d_3_a['X']
+y_a_3 = d_3_a['Y']
+z_a_3 = d_3_a['Z']
+d_4_a = abs_pos.query('CHANNEL == 3')
+x_a_4 = d_4_a['X']
+y_a_4 = d_4_a['Y']
+z_a_4 = d_4_a['Z']
+d_5_a = abs_pos.query('CHANNEL == 4')
+x_a_5 = d_5_a['X']
+y_a_5 = d_5_a['Y']
+z_a_5 = d_5_a['Z']
+d_6_a = abs_pos.query('CHANNEL == 5')
+x_a_6 = d_6_a['X']
+y_a_6 = d_6_a['Y']
+z_a_6 = d_6_a['Z']
+d_7_a = abs_pos.query('CHANNEL == 6')
+x_a_7 = d_7_a['X']
+y_a_7 = d_7_a['Y']
+z_a_7 = d_7_a['Z']
+d_8_a = abs_pos.query('CHANNEL == 7')
+x_a_8 = d_8_a['X']
+y_a_8 = d_8_a['Y']
+z_a_8 = d_8_a['Z']
+
+#detector scatter
+d_1_s = scatter_pos.query('CHANNEL == 0')
+x_s_1 = d_1_s['X']
+y_s_1 = d_1_s['Y']
+z_s_1 = d_1_s['Z']
+d_2_s = scatter_pos.query('CHANNEL == 1')
+x_s_2 = d_2_s['X']
+y_s_2 = d_2_s['Y']
+z_s_2 = d_2_s['Z']
+d_3_s = scatter_pos.query('CHANNEL == 2')
+x_s_3 = d_3_s['X']
+y_s_3 = d_3_s['Y']
+z_s_3 = d_3_s['Z']
+d_4_s = scatter_pos.query('CHANNEL == 3')
+x_s_4 = d_4_s['X']
+y_s_4 = d_4_s['Y']
+z_s_4 = d_4_s['Z']
+d_5_s = scatter_pos.query('CHANNEL == 4')
+x_s_5 = d_5_s['X']
+y_s_5 = d_5_s['Y']
+z_s_5 = d_5_s['Z']
+d_6_s = scatter_pos.query('CHANNEL == 5')
+x_s_6 = d_6_s['X']
+y_s_6 = d_6_s['Y']
+z_s_6 = d_6_s['Z']
+d_7_s = scatter_pos.query('CHANNEL == 6')
+x_s_7 = d_7_s['X']
+y_s_7 = d_7_s['Y']
+z_s_7 = d_7_s['Z']
+d_8_s = scatter_pos.query('CHANNEL == 7')
+x_s_8 = d_8_s['X']
+y_s_8 = d_8_s['Y']
+z_s_8 = d_8_s['Z']
 
 #plot of the frequecies of detections of photons at each detector 
 freqs = np.array([len(d_1), len(d_2), len(d_3), len(d_4), len(d_5), len(d_6), len(d_7), len(d_8)])
@@ -91,24 +162,6 @@ for ct in [0, 2, 5, 7, 9]:
                       level=100, 
                       cmap=plt.cm.RdYlBu_r, 
                       alpha=0.5)
-# ax4.view_init(0, -90)
-# x_centers = (edges[0][1:] + edges[0][:-1]) / 2
-# y_centers = (edges[1][1:] + edges[1][:-1]) / 2
-# z_centers = (edges[2][1:] + edges[2][:-1]) / 2
-# fig4 = plt.figure()
-# ax4 = fig4.add_subplot(111, projection='3d')
-
-# # Create a meshgrid of the bin centers for each axis
-# x, y, z = np.meshgrid(x_centers, y_centers, z_centers, indexing='ij')
-
-# # Flatten the histogram and the meshgrid
-# hist_flat = hist_.flatten()
-# x_flat = (X*50)/184
-# y_flat = (Y*50)/184
-# z_flat = (Z*50)/184
-
-# # Create the 3D density heat map using the scatter() function
-# ax4.scatter(x_flat, y_flat, z_flat, c=hist_flat, marker='o')
 
 # # Set the axis labels
 ax4.set_xlabel('X')
@@ -130,41 +183,77 @@ def hist(x, y, bins): #2d histogram of X and Y positions
     # yedges = np.linspace(min(y), max(y))
     n, x_edges, y_edges = np.histogram2d(x, y, bins)#=(xedges, yedges))
     return n
-h = hist(d_5['X'], d_5['Y'], 20)
+h = hist(d_5['X'], d_5['Y'], 50)
 fig6 = plt.figure()
 ax6 = fig6.add_subplot(111)
 ax6.imshow(h.T)
 
 #using gaussian_kde fucntion for density distribution
-xyz = np.vstack([d_1['X'],d_1['Y'],d_1['Z']])
-kde = stats.gaussian_kde(xyz)
-density = kde(xyz)
-fig7 = plt.figure()
-ax7 = fig7.add_subplot(111, projection = '3d')
-scatter = ax7.scatter(d_1['X'], d_1['Y'], d_1['Z'], c = density)
-fig7.colorbar(scatter)
-max_pos = xyz.T[np.argmax(density)]
 
-# def plot4d(data):
-#     fig = plt.figure(figsize=(5, 5))
-#     ax = fig.add_subplot(projection="3d")
-#     ax.xaxis.pane.fill = False
-#     ax.yaxis.pane.fill = False
-#     ax.zaxis.pane.fill = False
-#     mask = data > 0.01
-#     idx = np.arange(int(np.prod(data.shape)))
-#     x, y, z = np.unravel_index(idx, data.shape)
-#     ax.scatter(x, y, z, c=data.flatten(), s=10.0 * mask, edgecolor="face", alpha=0.2, marker="o", cmap="magma", linewidth=0)
-#     plt.tight_layout()
-#     plt.savefig("test_scatter_4d.png", dpi=250)
-#     plt.close(fig)
+def plot_density(d1, d2, d3):
+    xyz = np.vstack([d1, d2, d3])
+    kde = stats.gaussian_kde(xyz)
+    density = kde(xyz)
+    fig7 = plt.figure()
+    ax7 = fig7.add_subplot(111, projection = '3d')
+    scatter = ax7.scatter(d1, d2, d3, c = density)
+    fig7.colorbar(scatter, location='left')
+    max_pos = xyz.T[np.argmax(density)]
+    ax7.set_xlabel('X')
+    ax7.set_ylabel('Y')
+    ax7.set_zlabel('Z')
+    fig7.savefig('absorber.pdf', format='pdf', dpi=1000, bbox_inches='tight')
+    plt.show()
+    return max_pos
 
+# print(plot_density(x_s_1, y_s_1, z_s_1),
+# plot_density(x_a_1, y_a_1, z_a_1),
+# plot_density(x_s_2, y_s_2, z_s_2),
+# plot_density(x_a_2, y_a_2, z_a_2),
+# plot_density(x_s_3, y_s_3, z_s_3),
+# plot_density(x_a_3, y_a_3, z_a_3),
+# plot_density(x_s_4, y_s_4, z_s_4),
+# plot_density(x_a_4, y_a_4, z_a_4),
+# plot_density(x_s_5, y_s_5, z_s_5),
+# plot_density(x_a_5, y_a_5, z_a_5),
+# plot_density(x_s_6, y_s_6, z_s_6),
+# plot_density(x_a_6, y_a_6, z_a_6),
+# plot_density(x_s_7, y_s_7, z_s_7),
+# plot_density(x_a_7, y_a_7, z_a_7),
+# plot_density(x_s_8, y_s_8, z_s_8),
+# plot_density(x_a_8, y_a_8, z_a_8))
 
-# if __name__ == "__main__":
-#     # X = np.arange(-10, 10, 0.5)
-#     # Y = np.arange(-10, 10, 0.5)
-#     # Z = np.arange(-10, 10, 0.5)
-#     X, Y, Z = np.meshgrid(X, Y, Z, indexing="ij")
-#     density_matrix = np.sin(np.sqrt(X**2 + Y**2 + Z**2))
-#     plot4d(density_matrix)
+print(plot_density(x_a, y_a, z_a))
 
+table1 = [['Channel Number', 'Scatterer/Absorber', 'Highest Scattering Position', 'Highest Absorption Position'], 
+          ['0', 'Scatterer', plot_density(x_s_1, y_s_1, z_s_1), plot_density(x_a_1, y_a_1, z_a_1)],
+          ['1', 'Scatterer', plot_density(x_s_2, y_s_2, z_s_2), plot_density(x_a_2, y_a_2, z_a_2)],
+          ['2', 'Scatterer', plot_density(x_s_3, y_s_3, z_s_3), plot_density(x_a_3, y_a_3, z_a_3)],
+          ['3', 'Scatterer', plot_density(x_s_4, y_s_4, z_s_4), plot_density(x_a_4, y_a_4, z_a_4)],
+          ['4', 'Absorber', plot_density(x_s_5, y_s_5, z_s_5), plot_density(x_a_5, y_a_5, z_a_5)],
+          ['5', 'Absorber', plot_density(x_s_6, y_s_6, z_s_6), plot_density(x_a_6, y_a_6, z_a_6)],
+          ['6', 'Absorber', plot_density(x_s_7, y_s_7, z_s_7), plot_density(x_a_7, y_a_7, z_a_7)],
+          ['7', 'Absorber', plot_density(x_s_8, y_s_8, z_s_8), plot_density(x_a_8, y_a_8, z_a_8)]]
+
+# print(tabulate(table1, headers='firstrow'))
+with open('positionstable.txt', 'w') as f:
+    f.write(tabulate(table1))
+    
+table2= [['Channel Number', 'Scatterer/Absorber', 'Number of Scatters', 'Number of Absorptions'],
+          ['0', 'Scatterer', len(x_s_1), len(x_a_1)],
+          ['1', 'Scatterer', len(x_s_2), len(x_a_2)],
+          ['2', 'Scatterer', len(x_s_3), len(x_a_3)],
+          ['3', 'Scatterer', len(x_s_4), len(x_a_4)],
+          ['4', 'Absorber', len(x_s_5), len(x_a_5)],
+          ['5', 'Absorber', len(x_s_6), len(x_a_6)],
+          ['6', 'Absorber', len(x_s_7), len(x_a_7)],
+          ['7', 'Absorber', len(x_s_8), len(x_a_8)]]
+
+print(tabulate(table2, headers='firstrow'))
+with open('numberofeventtypes.txt', 'w') as f:
+    f.write(tabulate(table2))
+    
+
+fig8 = plt.figure()
+ax8 = fig8.add_subplot(111)
+ax8.scatter(d_7['X'], d_7['Y'])
