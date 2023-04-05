c1f759c683d2faa6aba1161f1522c5f4fd94fc6c
joeol <joeoliverevans@gmail.com>
2023-04-05 22:21:35 +0100

    for oracle

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -227,7 +227,7 @@ def save_matrix(voxelcube):
 if __name__ == '__main__':
     """reading in results from csv"""
     pairs = []
-    file_name = 'experimentalabsorptionscatter15thMarAllPoints.parquet'
+    file_name = 'experimentalscatterscatter15thMarAllPoints.parquet'
     df = pd.read_parquet(
         fr'{file_name}')
 
