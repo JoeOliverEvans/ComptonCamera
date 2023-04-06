71ead1fae45613a4bff76d1da692467b2d41b6b3
joeol <joeoliverevans@gmail.com>
2023-04-06 15:23:29 +0100

    24 2 source absorb scat array

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -231,7 +231,7 @@ def save_matrix(voxelcube):
 if __name__ == '__main__':
     """reading in results from csv"""
     pairs = []
-    file_name = 'experimentalscatterscatter24thMarNewGeometry2Source.parquet'
+    file_name = 'experimentalabsorptionscatter24thMarNewGeometry2Source.parquet'
     df = pd.read_parquet(
         fr'{file_name}')
 
