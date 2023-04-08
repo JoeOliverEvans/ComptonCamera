9bcd500c9bb1a86ca6c68c868717aac4a47915e2
joeol <joeoliverevans@gmail.com>
2023-04-08 16:28:48 +0100

    another monte part 2 for oracle

--- Image Reconstruction/Trying3DGeometry.py
+++ Image Reconstruction/Trying3DGeometry.py
@@ -233,7 +233,7 @@ def save_matrix(voxelcube):
 if __name__ == '__main__':
     """reading in results from csv"""
     pairs = []
-    file_name = 'posscatterscatter24thMar2SourceLikeExperimental.parquet'
+    file_name = 'posabsorptionscatter24thMar2SourceLikeExperimental.parquet'
     df = pd.read_parquet(
         fr'{file_name}')
 
